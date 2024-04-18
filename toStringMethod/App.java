edceb850bc57648d59e2548d06c41696e6e28b3d
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2024-04-17 21:26:37 -0500

    Add files via upload

---
+++ toStringMethod/App.java
@@ -0,0 +1,20 @@
+package toStringMethod;
+
+class Frog{
+    private int iD;
+    private String name;
+    public Frog(int iD, String name){
+        this.iD = iD;
+        this.name = name;
+    }
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(iD).append(": ").append(name);
+        return sb.toString();
+    }
+}
+public class App {
+    public static void main(String[] args) {
+        Frog frog1 = new Frog();
+    }
+}
