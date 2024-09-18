7d99d19a65539d322da7c16852efa6e82c548435
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2024-09-18 16:10:39 -0500

    Add files via upload

---
+++ ClassExamplesFall2024/BooleanExpressionsIfStatements/BooleanExpressions.java
@@ -0,0 +1,39 @@
+public class BooleanExpressions {
+    //Boolean is a primitive data type that
+    //allows you to get a true or false value
+
+    //Can use Relational Operators to store true/false information
+    /*
+    == equals
+    != not equals
+    < less than
+    > greater than
+    <= less than or Equal to
+    >= greater than or Equal to
+
+     */
+    public static void main(String[] args) {
+        int x = 25;
+        int y = 35;
+        System.out.println(x > y);
+
+        /*
+        You can use relational operators to compare the primitive data
+        types int, double, boolean and char
+         */
+
+        /*
+        int grade = 80;
+        boolean hasAnA = //write a boolean expression that evaluates true
+                           if grade is above 89
+         boolean hasAnA = grade > 89;
+
+        char key = ‘A’;
+        boolean isCorrectKey = //write a boolean expression
+                                that evaluates true if key is ‘Q’.
+        boolean isCorrectKey = key == ‘Q’;
+         */
+
+
+    }
+}
