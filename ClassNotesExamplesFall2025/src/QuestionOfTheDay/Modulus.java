1b74db3468dce287cd4a914ecd40abb225dea9fa
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2025-11-21 14:29:40 -0600

    Add files via upload

---
+++ ClassNotesExamplesFall2025/src/QuestionOfTheDay/Modulus.java
@@ -0,0 +1,12 @@
+package QuestionOfTheDay;
+
+public class Modulus
+{
+    public static void main(String[] args)
+    {
+        int first = 19;
+        int second = 5;
+        int result = first % second;
+        System.out.println(result);
+    }
+}
---
+++ ClassNotesExamplesFall2025/src/QuestionOfTheDay/MyProgram.java
@@ -0,0 +1,12 @@
+package QuestionOfTheDay;
+
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        final int myNum = 1;
+        int myNumTwo = 2;
+        myNumTwo = myNum + myNum + myNumTwo;
+        System.out.print(myNum + myNumTwo);
+    }
+}
