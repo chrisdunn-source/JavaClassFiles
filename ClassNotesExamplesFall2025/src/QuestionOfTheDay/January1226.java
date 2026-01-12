c09af9dad42304de283d2930e25c532de3422c4d
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2026-01-12 08:19:56 -0600

    Create January1226.java

---
+++ ClassNotesExamplesFall2025/src/QuestionOfTheDay/January1226.java
@@ -0,0 +1,11 @@
+public class January1226 {
+    public static void main(String[] args) {
+        int[] scores = {12, 81, 35, 73, 64, 55};
+
+        for(int i = 0; i < scores.length; i++)
+        {
+            System.out.println(scores[i-1]);
+        }
+        //This will be an error because the first iteration of the loop will print try to print out scores[-1] which does not exist and will cause an index out of bounds exception
+    }
+}
