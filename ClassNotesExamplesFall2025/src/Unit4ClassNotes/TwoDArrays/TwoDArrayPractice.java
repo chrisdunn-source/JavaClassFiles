a649eefaf23fcb8a13d27c5bcd0d17d5db0e623e
chrisdunn-source <103603068+chrisdunn-source@users.noreply.github.com>
2026-02-02 10:35:59 -0600

    Add files via upload

---
+++ ClassNotesExamplesFall2025/src/Unit4ClassNotes/TwoDArrays/TwoDArrayPractice.java
@@ -0,0 +1,25 @@
+package Unit4ClassNotes.TwoDArrays;
+
+public class TwoDArrayPractice {
+    public static void main(String[] args) {
+        int[] quiz1 = {90, 85, 77, 61, 94, 89};
+        int[] quiz2 = {94, 80, 85, 75, 88, 80};
+        int[] quiz3 = {93, 81, 80, 78, 90, 85};
+        int[] quiz4 = {89, 87, 82, 81, 96, 90};
+
+
+
+
+        //type[][] arrayName = {array1, array2, array3, ... };
+
+
+        int [][] quizzes = {{90, 85, 77, 61, 94, 89},
+                            {94, 80, 85, 75, 88, 80},
+                            {93, 81, 80, 78, 90, 85},
+                            {89, 87, 82, 81, 96, 90}};
+        int [][] quizzes2 = {quiz1,
+                             quiz2,
+                             quiz3,
+                             quiz4};
+    }
+}
